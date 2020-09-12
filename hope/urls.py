36d619f1b8c1f309f84e7e8cd26597fc87a7680b
eliyajoseph7 <42994583+eliyajoseph7@users.noreply.github.com>
2020-09-12 17:56:05 +0300

    more urls added

--- hope/urls.py
+++ hope/urls.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views as v
 
+app_name = 'hope'
+
 urlpatterns = [
     path('', v.index, name='index'),
+    path('about', v.about_view, name='about'),
+    path('view', v.blog_view, name='view'),
+    path('contactMessage', v.contact_view, name='message')
 ]
