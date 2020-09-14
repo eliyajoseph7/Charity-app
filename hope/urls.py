29c9354483ac88f9929e9fdac8e86c9d12a6d5f2
eliyajoseph7 <42994583+eliyajoseph7@users.noreply.github.com>
2020-09-14 20:38:44 +0300

    more urls added

--- hope/urls.py
+++ hope/urls.py
@@ -8,6 +8,8 @@ app_name = 'hope'
 urlpatterns = [
     path('', v.index, name='index'),
     path('about', v.about_view, name='about'),
-    path('view', v.blog_view, name='view'),
-    path('contactMessage', v.contact_view, name='message')
+    path('view/<slug:slug>', v.blog_view, name='view'),
+    path('contactMessage', v.contact_view, name='message'),
+    path('portfolios', v.portfolio_view, name='portfolios'),
+    path('about-me', v.about_me, name='about_me'),
 ]
