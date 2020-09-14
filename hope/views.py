0ad64d851f531b8eafa2d608d91443877d73b1ba
eliyajoseph7 <42994583+eliyajoseph7@users.noreply.github.com>
2020-09-14 20:39:39 +0300

    more views created

--- hope/views.py
+++ hope/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Contact
+from .models import Contact, Portfolio
 
 # Create your views here.
 def index(request):
-    return render(request, 'hope/pages/index.html')
+    portfolios = Portfolio.objects.all().order_by('-id')[:9]
+    return render(request, 'hope/pages/index.html', {'portfolios': portfolios})
     
 def about_view(request):
 
     return render(request, 'hope/pages/blog/about.html')    
 
-def blog_view(request):
-
-    return render(request, 'hope/pages/blog/blog.html')
+def blog_view(request, slug):
+    portfolio = Portfolio.objects.get(slug=slug)
+    related = Portfolio.objects.exclude(slug=slug)[:4]
+    return render(request, 'hope/pages/blog/blog.html', {'portfolio': portfolio, 'related': related})
 
 def contact_view(request):
     if request.method == 'POST':
@@ -25,3 +27,12 @@ def contact_view(request):
         contact.save()
 
         return HttpResponse("OK")
+
+
+def portfolio_view(request):
+    portfolio = Portfolio.objects.all()
+    return render(request, 'hope/pages/blog/allPortfolios.html', {'portfolio': portfolio})        
+
+
+def about_me(request):
+    return render(request, 'hope/pages/aboutMe.html')    
