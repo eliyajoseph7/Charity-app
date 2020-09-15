d3cbaa7b9bef639185b0f94d7fb20a2656271679
eliyajoseph7 <42994583+eliyajoseph7@users.noreply.github.com>
2020-09-16 00:35:04 +0300

    database engine modified

--- ThereIsHope/settings.py
+++ ThereIsHope/settings.py
@@ -141,9 +141,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Simplified static file serving.
